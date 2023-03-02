db1ab3c5ab5a536af61dec87e8dca4695e0e542e
rootM3eX <maulana.arshavien@gmail.com>
2023-03-03 05:36:41 +0700

    laravel & reactJS

---
+++ resources/js/Pages/Homepages.jsx
@@ -0,0 +1,39 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Homepages(props) {
+    return (
+            <>
+                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                    {props.auth.user ? (
+                        <Link
+                            href={route('dashboard')}
+                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route('login')}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route('register')}
+                                className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+                <div className='flex justify-center items-center min-h-screen bg-neutral-700
+                    text-white text-2xl'>
+                    <Head title={props.title}/>
+                    <h4>{props.description}</h4>
+                </div>
+            </>
+    );
+}
